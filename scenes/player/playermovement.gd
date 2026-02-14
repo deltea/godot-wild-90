ee95de42e436ca0412832e112abac5366a1ef09a
deltea <thcheetah777@gmail.com>
2026-02-14 10:25:51 -0800

    basic melee attack

--- scenes/player/playermovement.gd
+++ scenes/player/playermovement.gd
@@ -1,41 +1,53 @@
 extends CharacterBody2D
 
-var lookingRight = true
 @export var speed = 10
-var dashTime = 0
-var dashing = false
 @export var dashSpeed = 2
 
+var lookingRight = true
+var dashTime = 0
+var dashing = false
+var weaponDir = 1
 
-	
+@onready var anchor: Node2D = $Anchor
+@onready var weaponAnchor: Node2D = $Anchor/WeaponAnchor
+@onready var weaponRotDynamics: DynamicsSolver = Dynamics.create_dynamics(8.0, 0.8, 2.0)
+
+func _process(dt: float) -> void:
+	var dir = (get_global_mouse_position() - position).normalized()
+	anchor.rotation = dir.angle() + PI/2
+	weaponAnchor.rotation_degrees = weaponRotDynamics.update(weaponDir * 120)
+
+	if Input.is_action_just_pressed("click"):
+		weaponDir *= -1
+
 func _physics_process(delta: float) -> void:
-	#simple movement
+	# simple movement
 	var InputDir = Input.get_vector("left","right", "up", "down")
 	velocity += InputDir * speed
-	
-	#when dashing
-	if(dashTime > 7 || dashing == false):
-		velocity*=0.9
-	if Input.is_action_just_pressed("right"):
+
+	# when dashing
+	if (dashTime > 7 || dashing == false):
+		velocity *= 0.9
+	if (Input.is_action_just_pressed("right")):
 		lookingRight = true
-	if Input.is_action_just_pressed("left"):
+	if (Input.is_action_just_pressed("left")):
 		lookingRight = false
-		
-	
-	if(Input.is_action_just_pressed("dash") && dashing == false):
+
+
+	if (Input.is_action_just_pressed("dash") && dashing == false):
 		dashing = true
-	
-	if(dashing == true):
-		if(dashTime == 0):
-			velocity*=dashSpeed
-		dashTime+=1
-	if(dashTime == 40):
+
+	if (dashing == true):
+		if (dashTime == 0):
+			velocity *= dashSpeed
+		dashTime += 1
+	if (dashTime == 40):
 		dashTime = 0
 		dashing = false
-		
-	if(dashing == true && dashTime <= 15):
+
+	if (dashing == true && dashTime <= 15):
 		$Sprite.self_modulate.a = 0.5
 	else:
 		$Sprite.self_modulate.a = 1
-		
+
 	move_and_slide()
