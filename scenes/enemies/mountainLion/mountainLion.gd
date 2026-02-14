0a469deed08444f83f1da9e564a08790557f92bd
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-14 12:54:01 -0800

    mountain lion basic behavior

--- scenes/enemies/mountainLion/mountainLion.gd
+++ scenes/enemies/mountainLion/mountainLion.gd
@@ -1,7 +1,23 @@
 extends Enemy
 
 var state = "idle"
+var agroRange = 200
+var speed = 20
 
 func _physics_process(delta: float) -> void:
 	if state == "attack":
-		RoomManager.current_room.get_node("Player").position
+		var playerPos = RoomManager.current_room.get_node("Player").position
+		linear_velocity = (playerPos-position).normalized() * speed
+		
+
+
+func stateUpdate() -> void:
+	var moodSwing = randi_range(0,100)
+	var playerPos = RoomManager.current_room.get_node("Player").position
+	if position.distance_to(playerPos) < agroRange:
+		if moodSwing < 50:
+			state = "attack"
+	else:
+		if moodSwing < 80:
+			state = "idle"
+		
