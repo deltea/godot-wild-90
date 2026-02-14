ae598c56837afa5ab14ca7af7d3e7c1e695415c0
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-14 13:11:16 -0800

    mountain lions take knockback + stop at attack range

--- scenes/enemies/mountainLion/mountainLion.gd
+++ scenes/enemies/mountainLion/mountainLion.gd
@@ -1,21 +1,35 @@
 extends Enemy
 
 var state = "idle"
+
 var agroRange = 200
+var attackRange = 60
+
 var speed = 20
+
+var idlePosition = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if state == "attack":
 		var playerPos = RoomManager.current_room.get_node("Player").position
-		linear_velocity = (playerPos-position).normalized() * speed
+		var distanceToPlayer =  playerPos.distance_to(position)
 		
+		#get closer
+		if distanceToPlayer > attackRange:
+			linear_velocity = (playerPos-position).normalized() * speed
+		else:
+			linear_velocity = Vector2.ZERO
+
+func knockback(force):
+	super.knockback(force)
+	state = "recover"
 
 
 func stateUpdate() -> void:
 	var moodSwing = randi_range(0,100)
 	var playerPos = RoomManager.current_room.get_node("Player").position
 	if position.distance_to(playerPos) < agroRange:
-		if moodSwing < 50:
+		if moodSwing < 80:
 			state = "attack"
 	else:
 		if moodSwing < 80:
