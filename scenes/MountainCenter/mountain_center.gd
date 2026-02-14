b8983cb4b4effa6fe5d74cd88ab7122ecc4cdd49
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-13 23:33:49 -0800

    elevation adjusted for rotations

--- scenes/MountainCenter/mountain_center.gd
+++ scenes/MountainCenter/mountain_center.gd
@@ -4,6 +4,10 @@ var player
 
 #in degrees
 var theta = 0
+var elevation = 0
+var maxElevation = 1000
+var fullRotations = 0
+
 
 func _ready() -> void:
 	player = get_parent().get_node("player")
@@ -12,5 +16,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var dx = player.position.x - position.x
 	var dy = player.position.y - position.y
-	theta = rad_to_deg(-atan2(dy,dx))+180
-	print(theta)
+	
+	var lastTheta = theta
+	
+	theta = int(rad_to_deg(-atan2(dy,dx))+180)
+
+	if theta<10 and (lastTheta > 180):
+		fullRotations+=1
+	if theta>350 and lastTheta < 180:
+		fullRotations-=1
+		
+	elevation = theta+(360*fullRotations)
+	print(elevation)
+	
