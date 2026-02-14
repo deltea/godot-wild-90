d5e5a149c0ca9e23e72e6ff9d4923b4b33d27011
deltea <thcheetah777@gmail.com>
2026-02-13 18:29:51 -0800

    drone flashlight!

--- scenes/drone/drone.gd
+++ scenes/drone/drone.gd
@@ -4,10 +4,16 @@ class_name Drone extends CharacterBody2D
 @export var deceleration = 100.0
 @export var acceleration = 100.0
 
+@onready var pivot: Node2D = $Pivot
+
 var can_move = false
 
 func _enter_tree() -> void:
 	RoomManager.current_room.drone = self
+
+func _process(dt: float) -> void:
+	var dir = (get_global_mouse_position() - position).normalized()
+	pivot.rotation = dir.angle() - PI/2
 
 func _physics_process(delta):
 	var input = Input.get_vector("left", "right", "up", "down")
