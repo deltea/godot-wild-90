c7501d1714f7562a90fdc516e8489dca9d7a5b7b
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-14 18:31:24 -0800

    cell coloring based on mouse position

--- scenes/backpack/backpackManager.gd
+++ scenes/backpack/backpackManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var backpackSpace = []
+var backpackCells = []
 var rows = 5
 var columns = 4
 
@@ -17,8 +18,19 @@ func _ready() -> void:
 		
 	#place cells
 	for r in range(rows):
+		var newRow = []
 		for c in range(columns):
 			var cell = bpCell.instantiate()
 			$CanvasLayer.add_child(cell)
 			cell.position = Vector2(c*cellWidth,r*cellWidth)
+			newRow.append(cell)
+		backpackCells.append(newRow)
+			
+func _process(delta: float) -> void:
+	var mousePos = get_global_mouse_position()
+	for r in backpackCells:
+		for cell in r:
+			var cellPos = Vector2(cell.position.x + cellWidth/2, cell.position.y + cellWidth/2)
+			if cellPos.distance_to(mousePos) < 10:
+				cell.color = Color.RED
 	
