18150cc7aff70e981592ee1352642f8577c582e7
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-16 13:51:47 -0800

    basic folliage spawning

--- scenes/MountainCenter/mountain_center.gd
+++ scenes/MountainCenter/mountain_center.gd
@@ -11,14 +11,38 @@ var maxElevation = 1000
 var fullRotations = 0
 var maxRotations = 10
 
+var folliage = preload("res://scenes/folliage/folliage.tscn")
+
 func _ready() -> void:
 	player = RoomManager.current_room.player
 
+func spawnFolliage(num):
+	print("plant")
+	for i in range(num):
+		var oppositeAngle = deg_to_rad(-(theta+180))
+		var newPlant = folliage.instantiate()
+		add_child(newPlant)
+		var r = 380
+		
+		#randomize
+		r += randf_range(-80,80)
+		oppositeAngle += randf_range(-0.15,0.15)
+		newPlant.frame = randi_range(0,1)
+		if randi_range(0,1) == 1:
+			newPlant.flip_H = true
+
+		newPlant.global_position.x = cos(oppositeAngle)*r
+		newPlant.global_position.y = sin(oppositeAngle)*r
+
 func _process(delta: float) -> void:
+	#print(theta)
 	var dx = player.position.x - position.x
 	var dy = player.position.y - position.y
 
 	var lastTheta = theta
+	
+	if Input.is_action_just_pressed("dash"):
+		spawnFolliage(20)
 
 	theta = int(rad_to_deg(-atan2(dy, dx)) + 180)
 
