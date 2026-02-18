80a719c5ae00bbc058aecbb591eb18e3e7fea6b9
deltea <thcheetah777@gmail.com>
2026-02-17 16:45:55 -0800

    better xp attraction

--- scenes/xp/xp.gd
+++ scenes/xp/xp.gd
@@ -3,20 +3,24 @@ class_name XP extends Area2D
 @export var value = 1
 
 var velocity = Vector2.ZERO
-var speed = 300
-var attractSpeed = 380
+var speed = 500
+var attractSpeed = 400
+var isAttracted = false
 
 func _ready() -> void:
 	var dir = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	velocity = dir * speed
+	velocity = dir * 350
+	await Clock.wait(0.75)
+	isAttracted = true
 
 func _process(dt: float) -> void:
-	# move towards the player
-	speed += attractSpeed * dt
-	if RoomManager.current_room.player:
-		var player_pos = RoomManager.current_room.player.position
-		var dir = (player_pos - position).normalized()
-		velocity += dir * speed * dt
+	if isAttracted:
+		# move towards the player
+		speed += attractSpeed * dt
+		if RoomManager.current_room.player:
+			var player_pos = RoomManager.current_room.player.position
+			var dir = (player_pos - position).normalized()
+			velocity += dir * speed * dt
 
 func _physics_process(delta):
 	position += velocity * delta
