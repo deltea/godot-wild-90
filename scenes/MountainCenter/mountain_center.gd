23ebbf6825e545913ecf4e5c9e743852a9932734
deltea <thcheetah777@gmail.com>
2026-02-19 22:56:09 -0800

    fix mountain lion spawning in the mountain

--- scenes/MountainCenter/mountain_center.gd
+++ scenes/MountainCenter/mountain_center.gd
@@ -20,7 +20,7 @@ var recordTheta = 0
 
 func _ready() -> void:
 	player = RoomManager.current_room.player
-	
+
 	#spawn initial folliage
 	for i in range(60):
 		spawnFolliage(10,i*6)
@@ -31,24 +31,24 @@ func spawnFolliage(num, angle):
 		var oppositeAngle = deg_to_rad(-(angle))
 		var color = Color.DARK_SEA_GREEN
 		var newPlant = folliage.instantiate()
-		
+
 		get_parent().get_node("folliageContainer").add_child(newPlant)
-		
+
 		#randomize
 		r += randf_range(-80,100)
 		oppositeAngle += randf_range(-0.15,0.15)
 		newPlant.frame = randi_range(0,1)
 		color.r += randf_range(-0.2,0.2)
-		
+
 		r*=scaleMod
 		newPlant.modulate = color
 		if randi_range(0,1) == 1:
 			newPlant.flip_h = true
-		
+
 		#set folliage position
 		newPlant.global_position.x = cos(oppositeAngle)*r
 		newPlant.global_position.y = sin(oppositeAngle)*r
-		
+
 		#old folliage culling
 		if len(folliageList) > 100:
 			for plant in folliageList:
@@ -57,8 +57,8 @@ func spawnFolliage(num, angle):
 						plant.queue_free()
 		newPlant.z_index = -9
 		folliageList.append(newPlant)
-		
-		
+
+
 
 func _process(delta: float) -> void:
 	#print(theta)
@@ -66,13 +66,13 @@ func _process(delta: float) -> void:
 	var dy = player.position.y - position.y
 
 	var lastTheta = theta
-	
-	
+
+
 
 	theta = int(rad_to_deg(-atan2(dy, dx)) + 180)
-	
-	
-		
+
+
+
 
 	if theta < 10 and (lastTheta > 180):
 		fullRotations += 1
@@ -82,7 +82,7 @@ func _process(delta: float) -> void:
 	elevation = theta + (360 * fullRotations)
 	var adjustedElevation = (elevation / (360.0 * maxRotations)) * 100.0
 	elevationBar.value = adjustedElevation
-	
+
 	scaleMod = 	(2.0-(adjustedElevation/100.0))/2
 	scale = Vector2.ONE * scaleMod
 
@@ -91,19 +91,19 @@ func folliageCheck() -> void:
 	if abs(recordTheta-theta)>3:
 		spawnFolliage(10,theta)
 	recordTheta=theta
-	
+
 func spawnEnemy(scene):
 	var enemy = scene.instantiate()
-	add_child(enemy)
+	RoomManager.current_room.add_child(enemy)
 	var r = 360
 	var oppositeAngle = deg_to_rad(-(theta))
-	
+
 	#randomize
 	r += randf_range(-80,100)
 	oppositeAngle += randf_range(-0.15,0.15)
-	
+
 	r*=scaleMod
-	
+
 	#set folliage position
 	enemy.global_position.x = cos(oppositeAngle)*r
 	enemy.global_position.y = sin(oppositeAngle)*r
