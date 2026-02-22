3149b61e117f1ad6f7df11e064e49efd8220fde3
arrowira <41136800+arrowira@users.noreply.github.com>
2026-02-22 00:47:57 -0800

    snow ball explosion particles

--- scenes/enemies/golem/golem_projectile.gd
+++ scenes/enemies/golem/golem_projectile.gd
@@ -4,6 +4,7 @@ var playerDir = Vector2.ZERO
 var speed = 4
 var parabolicOffset = 0
 var t = 0
+var explosion = preload("res://scenes/particles/snowBall_particles.tscn")
 # Called when the node enters the scene tree for the first time.
 func initializeVals():
 	playerDir = (RoomManager.current_room.get_node("Player").position-position).normalized()
@@ -16,6 +17,10 @@ func _physics_process(delta: float) -> void:
 	$Circle512.position.y=parabolicOffset
 	
 func blowUp():
+	var exp = explosion.instantiate() as CPUParticles2D
+	exp.position = position
+	exp.emitting = true
+	RoomManager.current_room.add_child(exp)
 	queue_free()
 
 func _on_timer_timeout() -> void:
